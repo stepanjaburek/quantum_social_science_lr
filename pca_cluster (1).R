cb560405db45fa9891ef04d522fe00b818ee4536
Štěpán Jabůrek <168758805+stepanjaburek@users.noreply.github.com>
2024-12-29 16:28:00 +0100

    Add files via upload

---
+++ pca_cluster (1).R
@@ -0,0 +1,143 @@
+###################################
+# PCA and cluster analysis
+###############################
+# Setup
+########
+install.packages("factoextra")
+install.packages("cluster")
+library(tidyverse)
+library(factoextra)
+library(readxl)
+library(cluster)
+
+setwd("C:/Users/stepa/downloads")
+data<-read_csv("0_kw_analysis.csv")
+metadata<-read_excel("pdf_list(1).xlsx")
+##############################
+# Data cleaning and manipulation
+###########################
+# Lookup if data and metadata have the same papers
+missing_file_data <- setdiff(data$filename, metadata$file_name)
+missing_file_data
+missing_file_metadata <- setdiff( metadata$file_name, data$filename)
+missing_file_metadata
+
+# Rename "Broekaert_2018" in metadata
+metadata$file_name[252] <- "Broekaert_2018_The Tacit ‘Quantum’ of Meeting the Aesthetic Sign; Contextualize, Entangle,2.pdf"
+data$filename[252] <- "Broekaert_2018_The Tacit ‘Quantum’ of Meeting the Aesthetic Sign; Contextualize, Entangle,2.pdf"
+# Delete duplicit "Yukalov et al. - 2018 - Information processing by networks of quantum deci.pdf"
+data <- data %>% slice(-1170)
+# Delete missing "Yilmaz - 2017 - Quantum cognition models of ethical decision-makin.pdf"
+data<-data %>% slice(-1166)
+# Delete missing "Park_2016_Decision-making &amp quantum mechanical models of cognitive processing.pdf"
+metadata<-metadata %>% slice(-892)
+
+# Exclude authors
+df<-data %>% select(-2,-3,-5,-17,-31)
+# Exclude social science fields
+df<-df %>% select(-27:-38)
+# Possibly exclude "quantum" and "quantization" for robustness checks
+df<-df %>% select(-18,-19)
+
+# Can I see some ID?
+df <- df %>%
+  mutate(id = row_number()) %>%
+  filter(rowSums(across(2:24)) > 0)
+metadata<-metadata %>% mutate(id=row_number())
+
+# minmax normalization
+#df_norm <- t(apply(df[,2:24], 1, function(x) (x - min(x)) / (max(x) - min(x))))
+
+######################
+# Data analysis
+#####################
+# PCA
+#filenames <- df$filename
+features <- df %>% select(-1,-25)
+#features <- df_norm
+
+# Run PCA
+pca_result <- prcomp(features, scale=TRUE)
+summary(pca_result)
+fviz_eig(pca_result, 
+  title = "Scree Plot of Principal Components",
+  xlab = "Principal Components",
+  ylab = "Percentage of explained variances")
+
+# Feature importance in components
+loadings <- data.frame(pca_result$rotation)
+top_loadings <- loadings %>%
+  mutate(feature = rownames(loadings)) %>%
+  gather(PC, loading, -feature) %>%
+  group_by(PC) %>%
+  arrange(desc(abs(loading))) %>%
+  slice_head(n = 5)
+top_loadings
+
+# PC1 and 2
+pc12_loadings<-top_loadings %>%
+  filter(PC %in% c("PC1", "PC2")) %>%
+  arrange(PC, desc(abs(loading)))
+pc12_loadings
+
+# PCA biplot
+fviz_pca_biplot(pca_result,
+  geom.ind = "point",  
+  label = "var",      
+  title = "PCA of Paper Concepts")
+
+############
+# Cluster analysis
+########
+set.seed(42)
+# Kmeans
+#n_clusters <- 3
+#km_result <- kmeans(pca_result$x[, 1:5], centers = n_clusters)
+
+# PAM
+pam_result <- pam(pca_result$x[, 1:5], k=3)
+fviz_cluster(pam_result, 
+             data = pca_result$x[, 1:3],
+             main = "PCA with PAM Clustering")
+
+
+# Get the centroid papers (here we keep 10 with ordering by distance)
+centroid_coords <- pam_result$medoids
+
+top_centroids <- lapply(1:nrow(centroid_coords), function(i) {
+  distances <- apply(pca_result$x[, 1:5], 1, function(x) 
+    sum((x - centroid_coords[i,])^2))
+  top_indices <- order(distances)[1:10]
+  data.frame(
+    cluster = i,
+    rank = 1:10,
+    centroid_id = df$id[top_indices],
+    distance = distances[top_indices]
+  )
+})
+
+centroids <- do.call(rbind, top_centroids)
+centroids
+
+# Bind the centroid papers with their respective metadata
+centerpapers <- left_join(centroids, metadata, by = c("centroid_id" = "id"))
+dataset<-left_join(data,metadata, by=c("filename" = "file_name"))
+############################################################################x
+library(party)
+require(flexplot)
+
+df_analysis <- df %>% 
+  select(-filename) %>%
+  rename_all(~gsub(" ", "", .))
+
+flexplot(politicalscience~internationalrelations,data=df_analysis,  jitter =1)
+set.seed(1010)
+rf_model = cforest(superposition ~  ., data = df_analysis)
+estimates(rf_model)
+
+predictions = compare.fits(superposition ~ entanglement + interference,   data = df_analysis,   rf_model,   return.preds = T)
+flexplot(superposition ~  entanglement + interference, data = df_analysis, prediction = predictions)
+
+flexplot(superposition~1,data=df_analysis)
+1+1
+
